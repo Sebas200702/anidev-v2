82e63ccc99867cb1f9b42538ba208e8561c88870
Sebas2007 <sebastorregroza6@gmail.com>
2026-03-31 08:53:17 -0500

    chore: add session middleware integration and update adapter to bun

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,29 @@ import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 
 import tailwindcss from '@tailwindcss/vite'
+import bun from '@nurodev/astro-bun'
 
-import node from '@astrojs/node'
+/** @type {import('astro').AstroIntegration} */
+const sessionMiddlewareIntegration = {
+  name: 'session-middleware',
+  hooks: {
+    'astro:config:setup': ({ addMiddleware }) => {
+      addMiddleware({
+        entrypoint: new URL(
+          './src/core/http/middlewares/session.ts',
+          import.meta.url
+        ),
+        order: 'pre',
+      })
+    },
+  },
+}
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  output: 'server',
+  integrations: [sessionMiddlewareIntegration, react()],
+  output: 'server', // indica que no es solo static
+  adapter: bun(),
 
   vite: {
     plugins: [tailwindcss()],
@@ -20,8 +36,4 @@ export default defineConfig({
       },
     },
   },
-
-  adapter: node({
-    mode: 'standalone',
-  }),
 })
