d3f8206506779d2b82c87f9e67e99f58e04af482
Sebas2007 <sebastorregroza6@gmail.com>
2026-03-31 20:36:50 -0500

    feat(anime): implement anime list service with caching and media retrieval

---
+++ src/domains/anime/services/anime-list.ts
@@ -0,0 +1,36 @@
+import { withCache } from '@/core/cache'
+import { animeListCache } from '@/domains/anime/cache/anime-list'
+import { mapAnimeListToCards } from '@/domains/anime/mappers/anime-card'
+import { mapAnimeFilters } from '@/domains/anime/mappers/anime-filters'
+import { animeListRepository } from '@/domains/anime/repositories/anime-list'
+import { animeMediaRepository } from '@/domains/anime/repositories/anime-media'
+import type {
+  AnimeFilters,
+  AnimeFiltersParams,
+} from '@/domains/anime/types/anime-list'
+export const animeListService = {
+  async getAnimeList(filtersParams: AnimeFiltersParams) {
+    return withCache({
+      key: animeListCache.key(mapAnimeFilters(filtersParams)),
+      getCache: () => animeListCache.get(mapAnimeFilters(filtersParams)),
+      setCache: (_, value) =>
+        animeListCache.set(mapAnimeFilters(filtersParams), value),
+      compute: async () => {
+        const filters: AnimeFilters = mapAnimeFilters(filtersParams)
+        const animeList = await animeListRepository.getAnimeList(filters)
+        const animeIds = animeList.map((anime) => anime.malId)
+        const mediaList =
+          await animeMediaRepository.getMediaByAnimeIds(animeIds)
+        const total = await animeListRepository.getAnimeListCount(filters)
+
+        return {
+          list: mapAnimeListToCards({
+            animeList,
+            mediaList,
+          }),
+          total,
+        }
+      },
+    })
+  },
+}
