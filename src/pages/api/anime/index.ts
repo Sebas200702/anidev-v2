cac08b315f8a23d1c9d4cb8c918ba2cb86fde1f2
Sebas2007 <sebastorregroza6@gmail.com>
2026-04-02 14:26:16 -0500

    fix(api): update GET handler to use animeListRequestSchema and correct query parameter access

--- src/pages/api/anime/index.ts
+++ src/pages/api/anime/index.ts
@@ -1,41 +1,42 @@
 import { mapErrorToHttp } from '@/core/errors/handle-error'
 import { withZodValidation } from '@/core/http/middlewares/with-zod-validation'
 import {
-  animeFiltersParamsSchema,
+  animeListRequestSchema,
   animeListResponseSchema,
 } from '@/domains/anime/schemas/anime-list'
 import { animeListService } from '@/domains/anime/services/anime-list'
 import type { APIRoute } from 'astro'
-export const GET: APIRoute = withZodValidation(animeFiltersParamsSchema)(
-  async ({ validated }) => {
-    try {
-      const { list: animeCards, total } =
-        await animeListService.getAnimeList(validated)
-      const payload = {
-        data: animeCards,
-        status: 200,
-        meta: {
-          page: validated.page,
-          total,
-          hasNext: validated.page * validated.limit < total,
-        },
-      }
-      const responseBody = animeListResponseSchema.parse(payload)
-      return new Response(JSON.stringify(responseBody), {
-        headers: { 'Content-Type': 'application/json' },
-      })
-    } catch (error) {
-      const { status, body } = mapErrorToHttp(error)
-      const payload = {
-        data: null,
-        status,
-        error: body.message ?? 'Unexpected error',
-        meta: body.meta ?? {},
-      }
-      return new Response(JSON.stringify(payload), {
-        status,
-        headers: { 'Content-Type': 'application/json' },
-      })
+export const GET: APIRoute = withZodValidation(animeListRequestSchema)(async ({
+  validated,
+}) => {
+  try {
+    const { list: animeCards, total } = await animeListService.getAnimeList(
+      validated.query
+    )
+    const payload = {
+      data: animeCards,
+      status: 200,
+      meta: {
+        page: validated.query.page,
+        total,
+        hasNext: validated.query.page * validated.query.limit < total,
+      },
     }
+    const responseBody = animeListResponseSchema.parse(payload)
+    return new Response(JSON.stringify(responseBody), {
+      headers: { 'Content-Type': 'application/json' },
+    })
+  } catch (error) {
+    const { status, body } = mapErrorToHttp(error)
+    const payload = {
+      data: null,
+      status,
+      error: body.message ?? 'Unexpected error',
+      meta: body.meta ?? {},
+    }
+    return new Response(JSON.stringify(payload), {
+      status,
+      headers: { 'Content-Type': 'application/json' },
+    })
   }
-)
+})
